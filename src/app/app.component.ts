cf774c3106b0d3084d0367a179e085ac97e00ddb
Mark Walsh <S00181377@mail.itsligo.ie>
2019-04-01 18:38:05 +0100

    Styles and Displayment changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GoogleBooksApiService } from './services/google-books-api.service';
+import { GoogleBookResponse } from 'src/GoogleBookResponse';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,18 @@ export class AppComponent {
 
   // Error Message Declaration
   errorMessage: any;
-  bookData : any;
+  bookData : GoogleBookResponse;
 
 
   constructor(private _googleBookAPIService: GoogleBooksApiService){
   }
   
   // 
-  getBookDetails(bookName: string): boolean {
+  getBookDetails(bookName: string): boolean { 
 
     this._googleBookAPIService.getBookData(bookName).subscribe(bookData => {
       this.bookData = bookData;
+      console.log("AppComp: BookData:"+JSON.stringify(bookData.items[2].volumeInfo));
     },
       error => this.errorMessage = <any>error);
     return false;
